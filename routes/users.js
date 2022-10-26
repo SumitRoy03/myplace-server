30dff3d07f0e2738ebc1e33eb31a8e2805326d33
SumitRoy03 <rsumit647@gmail.com>
2022-10-26 14:02:19 +0530

    server added

---
+++ routes/users.js
@@ -0,0 +1,28 @@
+import express from "express";
+import {
+  createUser,
+  updateUser,
+  deleteUser,
+  getUser,
+  getUsers,
+} from "../controllers/user.js";
+import { verifyAdmin, verifyUser } from "../utils/verifyToken.js";
+
+const router = express.Router();
+
+//CREATE
+router.post("/", createUser);
+
+//UDATE
+router.put("/:id", verifyUser, updateUser);
+
+//DELETE
+router.delete("/:id", verifyUser, deleteUser);
+
+//GET User BY ID
+router.get("/:id", verifyUser, getUser);
+
+//GET ALL Users
+router.get("/", verifyAdmin, getUsers);
+
+export default router;
